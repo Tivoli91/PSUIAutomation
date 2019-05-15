50edf124760d3aaf3287e3da9fe12182a55aa2ec
Tivoli91 <348001390@qq.com>
2019-05-15 19:58:17 +0800

    Create Initialize-UIAutomation.ps1

---
+++ Initialize-UIAutomation.ps1
@@ -0,0 +1,7 @@
+Function Initialize-UIAutomation(){
+    [void][System.Reflection.Assembly]::LoadWithPartialName("System.Windows.Forms") # for send key and clipboard clear use
+    [void][System.Reflection.Assembly]::LoadWithPartialName("UIAutomationClient")
+    [void][System.Reflection.Assembly]::LoadWithPartialName("UIAutomationTypes")
+    [void][System.Reflection.Assembly]::LoadWithPartialName("UIAutomationProvider")
+    [void][System.Reflection.Assembly]::LoadWithPartialName("UIAutomationClientsideProviders")
+}
